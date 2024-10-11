082f3851eeb7b0879257ab8c46ab54ed21e73e60
Tanya Yenakieva <yastigma@gmail.com>
2024-10-11 14:49:53 +0300

    task generics completed

---
+++ my-TS-project/src/generics/2.ts
@@ -0,0 +1,19 @@
+// У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. Ваше завдання – використовувати Pick та generics для вказівки, що поля цих параметрів належать AllType. Функція compare повинна повертати AllType.
+
+
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number
+}
+
+function compare (top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'> ): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
+}
---
+++ my-TS-project/src/generics/5.ts
@@ -0,0 +1,16 @@
+// У вас є перелік UserRole, який використовується для класифікації користувачів у вашому додатку. Ви хочете створити об'єкт RoleDescription, який зіставлятиме кожну роль користувача з її описом.
+
+export enum UserRole {
+  admin = 'admin',
+  editor = 'editor',
+  guest = 'guest',
+}
+
+// Замініть наступний код на версію за допомогою Record
+type RoleDescription = Record<UserRole, string>;
+
+const RoleDescription: RoleDescription = {
+  admin: 'Admin User',
+  editor: 'Editor User',
+  guest: 'Guest User',
+};
