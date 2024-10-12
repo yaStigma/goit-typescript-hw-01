31a02beb1cad4dd5801a7cdaf5b24f8309f4e661
Tanya Yenakieva <yastigma@gmail.com>
2024-10-12 14:52:06 +0300

    fix 3.ts generics

--- my-TS-project/src/generics/3.ts
+++ my-TS-project/src/generics/3.ts
@@ -2,7 +2,7 @@
 
 
 
-function merge<T extends object, U extends object>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
